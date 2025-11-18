8fb9b71711dfb9c835fc5c07762b2e21e8a4d791
myrrhex06 <gitlwer210@gmail.com>
2025-11-19 06:38:00 +0900

    feat: 할일 표시 TableView 구현

--- Todolune/Controller/ViewController.swift
+++ Todolune/Controller/ViewController.swift
@@ -1,13 +1,14 @@
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    private let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .white
-        
         setupNavigation()
+        setupTableView()
     }
     
     func setupNavigation(){
@@ -16,5 +17,45 @@ class ViewController: UIViewController {
         self.navigationItem.largeTitleDisplayMode = .always
         
         self.title = "Todolune"
+    }
+    
+    func setupTableView(){
+        self.view.addSubview(tableView)
+        
+        tableView.backgroundColor = UIColor(named: "backgroundColor")
+        
+        tableView.separatorColor = .gray
+        
+        tableView.dataSource = self
+        
+        tableView.register(TodoCell.self, forCellReuseIdentifier: "TodoCell")
+        
+        tableView.rowHeight = 100
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0),
+            tableView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0)
+        ])
+    }
+}
+
+extension ViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath) as! TodoCell
+        
+        cell.selectionStyle = .none
+        cell.createdAtLabel.text = "yesterday"
+        cell.todoTitleLabel.text = "Buy milk"
+        
+        return cell
     }
 }
