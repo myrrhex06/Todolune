55488d34d997b6b6a9904f34f7ae23d7cbb943f1
myrrhex06 <gitlwer210@gmail.com>
2025-11-22 12:13:17 +0900

    feat: placeholder에 사용되는 문자열 상수 설정

---
+++ Todolune/Common/Constant.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+final class Constant{
+    
+    private init() {}
+    
+    public static let TITLE_PLACEHOLDER = "What needs to be done?"
+    public static let DESCRIPTION_PLACEHOLDER = "Add details or notes..."
+}
