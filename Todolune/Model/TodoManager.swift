a97c1461cdc8113c80ca3c2c389c46230f419a07
myrrhex06 <gitlwer210@gmail.com>
2025-12-09 21:51:54 +0900

    feat: 할일 목록 limit, offset 프로퍼티 및 관련 메서드 추가

--- Todolune/Model/TodoManager.swift
+++ Todolune/Model/TodoManager.swift
@@ -8,10 +8,22 @@ final class TodoManager{
     
     private let coreDataManager = CoreDataManager.shared
     
-    func fetchTodos() -> [Todo]?{
-        let todo = coreDataManager.fetchTodoList()
+    private var todoList: [Todo] = []
+    
+    private var offset = 0
+    
+    private var limit = 10
+    
+    private var isDataEnded: Bool = false
+    
+    func fetchTodos() -> [Todo]? {
+        guard let todo = coreDataManager.fetchTodoList(offset: offset, limit: limit) else { return nil }
         
         return todo
+    }
+    
+    func clearTodo() {
+        todoList = []
     }
     
     func createTodo(title: String, description: String){
@@ -26,4 +38,37 @@ final class TodoManager{
         coreDataManager.updateTodo(parameterTodo: todo)
     }
     
+    func getTodoList() -> [Todo]{
+        return todoList
+    }
+    
+    func appendTodoList(todos: [Todo]?){
+        guard let todos = todos else { return }
+        
+        todoList.append(contentsOf: todos)
+    }
+    
+    func getOffset() -> Int{
+        return offset
+    }
+    
+    func getLimit() -> Int{
+        return limit
+    }
+    
+    func getIsDataEnded() -> Bool{
+        return isDataEnded
+    }
+    
+    func setIsDataEnded(isDataEnded: Bool){
+        self.isDataEnded = isDataEnded
+    }
+    
+    func setOffset(offset: Int){
+        self.offset = offset
+    }
+    
+    func setLimit(limit: Int){
+        self.limit = limit
+    }
 }
